b712987723dcf5598077588bad5b94e430846e76
rafael <rafaelvalmore21@gmail.com>
2024-11-06 19:40:02 -0500

    Taller-1 accessing data with JPA

---
+++ taller-1/src/main/java/com/organizacion/software/taller_1/Customer.java
@@ -0,0 +1,33 @@
+package com.organizacion.software.taller_1;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+}
